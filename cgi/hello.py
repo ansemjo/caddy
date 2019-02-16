4981f7ad33d1f1b3200a203c17ab0d2c36b412e5
Anton Semjonov <anton@semjonov.de>
2019-02-16 23:05:38 +0100

    add seperate cgi dockerfile
    feature was present on an older branch, now maintain both proxy and cgi
    images in one branch in seperate subdirectories instead

---
+++ cgi/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# use cgi debugging
+import cgitb
+cgitb.enable()
+
+# initialize cgi and url parameter parser
+import cgi
+url = cgi.FieldStorage()
+
+# generate formatted timestamp
+import datetime
+timestamp = datetime.datetime.now().strftime('%A, %d. %B %Y, %H:%M:%S')
+
+# greeting name
+name = url.getfirst("name", "World")
+
+# print header
+print("Content-Type: text/plain\n")
+
+# print sample file
+print(f"// {timestamp}\nHello, {name}!")
